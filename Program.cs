052d3c64d9994212fb9284f9dee0944438e6eced
Antanas Ramanauskas <antanas.r@isun.ch>
2024-10-16 22:18:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using WinBiometricDotNet;
+
+var session = WinBiometric.OpenSession();
+
+Console.WriteLine("Please use the biometric scanner to locate sensor");
+var unitId = WinBiometric.LocateSensor(session);
+
+while (true)
+{
+    try
+    {
+        var identifyResult = WinBiometric.Identify(session);
+        Console.WriteLine(JsonSerializer.Serialize(identifyResult));
+        break;
+    }
+    catch (WinBiometricException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+WinBiometric.CloseSession(session);
+
+
+Console.WriteLine("Please scan your Left Middle Finger");
+WinBiometric.BeginEnroll(session, FingerPosition.LeftMiddle, unitId);
+
+var captureEnrollResult = WinBiometric.CaptureEnroll(session);
+while (captureEnrollResult.IsRequiredMoreData || captureEnrollResult.RejectDetail != default)
+{
+    Console.WriteLine("Please scan your Left Middle Finger again for more data");
+    captureEnrollResult = WinBiometric.CaptureEnroll(session);
+}
+
+var biometricIdentity = WinBiometric.CommitEnroll(session);
+Console.WriteLine("Left Middle Finger enrolled successfully");
+Console.WriteLine(JsonSerializer.Serialize(biometricIdentity));
+
+Console.WriteLine("Scan to identify");
+
